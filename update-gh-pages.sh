9970701d8e6ef1263fc27f2645dbf58c8b43d859
Travis <travis@travis-ci.org>
2015-08-05 16:27:47 +0100

    Fixed repo URL

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -12,7 +12,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git config --global user.name "Travis"
 
   # using token clone gh-pages branch
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com:Ishino/batoru-royale.git gh-pages > /dev/null
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Ishino/batoru-royale.git gh-pages > /dev/null
 
   # go into directory and copy data we're interested in to that directory
   cd gh-pages
