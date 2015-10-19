3aec9f12f1ada7169688f89345f2afffcf1fe171
Dries Vanlerberghe <dries.vanlerberghe@gmail.com>
2015-10-19 22:46:13 +0100

    Celery
    Added first basis for Celery - might switch to socketIO

--- batoru/hello.py
+++ batoru/hello.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from celery import Celery
+from battle import Battle
 
 app = Flask(__name__)
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
 
 @app.route("/")
 def hello():
-    return "This is not another \"Hello World!\" app. Ask Vijay!"
+    return "Testing:"
+
+@celery.task(bind=True)
+def run_battle(self):
+    Battle()
 
 if __name__ == "__main__":
     app.run()
