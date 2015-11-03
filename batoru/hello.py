8e7849c68e14651d98974a42559759e44359f25e
Dries Vanlerberghe <dries.vanlerberghe@capgemini.com>
2015-11-03 18:47:54 +0000

    Updated requirements.txt and added Pandas

--- batoru/hello.py
+++ batoru/hello.py
@@ -9,9 +9,11 @@ app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
+
 @app.route("/")
 def hello():
     return "Testing:"
+
 
 @celery.task(bind=True)
 def run_battle(self):
