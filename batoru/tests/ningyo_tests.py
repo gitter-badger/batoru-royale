4ffe1859abe8704fde392c86b40ee60506fcdb8a
Dries Vanlerberghe <dries.vanlerberghe@capgemini.com>
2015-12-15 00:35:24 +0000

    Fix Ningyo unit tests

--- batoru/tests/ningyo_tests.py
+++ batoru/tests/ningyo_tests.py
@@ -25,22 +25,22 @@ class TestNingyo(unittest.TestCase):
 
     def test_empower(self):
         self.player.fightSkill = 1
-        self.player.empower(1)
+        self.player.empower(1, 1)
         self.assertEqual(self.player.fightSkill, 2)
 
     def test_weaken(self):
         self.player.fightSkill = 2
         self.player.hitPoints = 2
 
-        self.player.weaken(1, 1)
+        self.player.weaken(1, 1, 1)
         self.assertEqual(self.player.fightSkill, 1)
         self.assertEqual(self.player.hitPoints, 1)
 
-        self.player.weaken(1, 1)
+        self.player.weaken(1, 1, 1)
         self.assertEqual(self.player.fightSkill, 0)
         self.assertEqual(self.player.hitPoints, 0)
 
-        self.player.weaken(1, 1)
+        self.player.weaken(1, 1, 1)
         self.assertEqual(self.player.fightSkill, 0)
         self.assertEqual(self.player.hitPoints, -1)
 
@@ -50,6 +50,7 @@ class TestNingyo(unittest.TestCase):
         self.player.strengthMultiplier = 1
         self.player.offenceReduction = 1
         self.player.fightSkill = 1
+        self.player.ability = 0
         self.assertEqual(self.player.offence(), 1)
 
     def test_defence(self):
@@ -58,6 +59,7 @@ class TestNingyo(unittest.TestCase):
         self.player.staminaMultiplier = 1
         self.player.defenceReduction = 1
         self.player.fightSkill = 1
+        self.player.ability = 0
         self.assertEqual(self.player.defence(), 1)
 
 if __name__ == '__main__':
